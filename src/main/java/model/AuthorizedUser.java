380aec98546bf2d2d33142815133aa6f50f6d690
Dmytro Teliukov <77294463+DmytroTeliukov@users.noreply.github.com>
2022-10-13 11:43:21 +0300

    [Task-1] Created domain models (#2)

---
+++ src/main/java/model/AuthorizedUser.java
@@ -0,0 +1,6 @@
+package model;
+
+public class AuthorizedUser {
+    private String email;
+    private Role role;
+}
---
+++ src/main/java/model/Course.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.List;
+
+public class Course {
+    private Teacher teacher;
+    private List<Group> groups;
+    private List<Material> materials;
+    private List<Task> tasks;
+}
