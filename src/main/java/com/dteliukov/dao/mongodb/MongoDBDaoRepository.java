d0581f9e51eae09ac27bcbec64d5b4a1f292dcff
Dmytro Teliukov <77294463+DmytroTeliukov@users.noreply.github.com>
2022-11-09 20:12:48 +0200

    add mongo repository

---
+++ src/main/java/com/dteliukov/dao/mongodb/MongoDBDaoRepository.java
@@ -0,0 +1,20 @@
+package com.dteliukov.dao.mongodb;
+
+import com.dteliukov.dao.*;
+
+public class MongoDBDaoRepository extends DaoRepository {
+    @Override
+    public AnswerDao getAnswerDao() {
+        return null;
+    }
+
+    @Override
+    public CourseDao getCourseDao() {
+        return null;
+    }
+
+    @Override
+    public UserDao getUserDao() {
+        return null;
+    }
+}
